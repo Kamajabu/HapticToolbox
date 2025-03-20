4cd41ab6df79f97714c68f5ce376ae2141797e27
Kamil Buczel <kamil.buczel@kamajabu.pl>
2025-03-20 21:12:05 +0100

    Initial Commit

---
+++ Haptic Toolbox/Haptic_ToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptic_ToolboxApp.swift
+//  Haptic Toolbox
+//
+//  Created by Kamil Buczel on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Haptic_ToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
